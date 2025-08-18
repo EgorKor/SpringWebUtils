97d404480f20a6130c03b07314511e55ba509e35
Egor <egorka_lolka23@mail.ru>
2025-08-18 13:31:43 +0400

    feat: добавил типизацию для ChoicesSupplier

--- src/main/java/io/github/egorkor/webutils/meta/ChoicesSupplier.java
+++ src/main/java/io/github/egorkor/webutils/meta/ChoicesSupplier.java
@@ -3,6 +3,6 @@ package io.github.egorkor.webutils.meta;
 import java.util.List;
 
 @FunctionalInterface
-public interface ChoicesSupplier {
-    List<Object> getChoices();
+public interface ChoicesSupplier<T> {
+    List<T> getChoices();
 }
